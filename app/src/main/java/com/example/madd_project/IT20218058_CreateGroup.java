dfc30b3b40f24c7aa894cee72d3d4ac2c297081b
Sandali-R <it20218058@my.sliit.lk>
2021-08-09 23:35:07 +0530

    Project added to GIT

---
+++ app/src/main/java/com/example/madd_project/IT20218058_CreateGroup.java
@@ -0,0 +1,14 @@
+package com.example.madd_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IT20218058_CreateGroup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_it20218058_create_group);
+    }
+}
