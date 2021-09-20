196b058f8fa33ecb22b6619bcad6eebb2c709c91
SRHerath <79823718+SRHerath@users.noreply.github.com>
2021-09-20 17:25:26 +0530

    Commit one Sasini

--- app/src/main/java/com/example/madd_project/IT20226664_AddEvents.java
+++ app/src/main/java/com/example/madd_project/IT20226664_AddEvents.java
@@ -3,8 +3,12 @@ package com.example.madd_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class IT20226664_AddEvents extends AppCompatActivity {
+public class IT20226664_AddEvents extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,4 +19,40 @@ public class IT20226664_AddEvents extends AppCompatActivity {
 
         setContentView(R.layout.activity_it20226664_add_events);
     }
+
+
+
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+
+        popup.inflate(R.menu.popup_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.item4:
+                Toast.makeText(this, "Item 4 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return false;
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/madd_project/MainActivity.java
+++ app/src/main/java/com/example/madd_project/MainActivity.java
@@ -54,18 +54,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void open_module_page(View view) {
-        Intent intent = new Intent(this,IT20218058_CreateGroup.class);
+        Intent intent = new Intent(this,IT20255756_GradingModule.class);
         startActivity(intent);
     }
 
     public void open_event_page(View view) {
-        Intent intent = new Intent(this,IT20226664_AddEvents.class);
+        Intent intent = new Intent(this,IT20226664_MyEvents.class);
         startActivity(intent);
     }
 
     public void open_timetable_page(View view) {
-        Intent intent = new Intent(this,IT20218058_CreateGroup.class);
+        Intent intent = new Intent(this,IT20225674_UpdateTimeTable.class);
         startActivity(intent);
+
     }
 
     public void open_studyGroup_page(View view) {
