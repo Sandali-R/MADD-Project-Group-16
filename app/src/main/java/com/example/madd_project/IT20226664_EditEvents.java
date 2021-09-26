eb78cba69b7d694fe97431aebe040309c2ba1ae7
SRHerath <79823718+SRHerath@users.noreply.github.com>
2021-09-26 21:42:00 +0530

    new 11 commit sasini

---
+++ app/src/main/java/com/example/madd_project/IT20226664_EditEvents.java
@@ -0,0 +1,19 @@
+package com.example.madd_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IT20226664_EditEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //hide top action bar
+        getSupportActionBar().hide();
+
+
+        setContentView(R.layout.activity_it20226664_edit_events);
+    }
+}
---
+++ app/src/main/java/com/example/madd_project/IT20226664_confirm_delete.java
@@ -0,0 +1,16 @@
+package com.example.madd_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IT20226664_confirm_delete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_it20226664_confirm_delete);
+    }
+
+
+}
